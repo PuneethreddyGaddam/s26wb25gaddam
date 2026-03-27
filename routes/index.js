eb5c05d02c25a2240d10f8be9442e4aeb3d857a8
puneeth reddy gaddam <s561425@nwmissouri.edu>
2026-03-26 20:44:36 -0500

    Fix home route without view engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send(
+    '<!doctype html>' +
+      '<html lang="en">' +
+      '<head>' +
+      '<meta charset="utf-8" />' +
+      '<meta name="viewport" content="width=device-width, initial-scale=1" />' +
+      '<title>Express</title>' +
+      '</head>' +
+      '<body>' +
+      '<h1>Express</h1>' +
+      '<p>Server is running.</p>' +
+      '</body>' +
+      '</html>'
+  );
 });
 
 module.exports = router;

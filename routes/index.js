f8e37116f1750e2b1e11da4681672041895b57e8
puneeth reddy gaddam <s561425@nwmissouri.edu>
2026-04-01 12:12:07 -0500

    completed through screenshot 3

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send(
-    '<!doctype html>' +
-      '<html lang="en">' +
-      '<head>' +
-      '<meta charset="utf-8" />' +
-      '<meta name="viewport" content="width=device-width, initial-scale=1" />' +
-      '<title>Express</title>' +
-      '</head>' +
-      '<body>' +
-      '<h1>Express</h1>' +
-      '<p>Server is running.</p>' +
-      '</body>' +
-      '</html>'
-  );
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

c196c3d87b5d0edcc8b7e6006ba09da61c53d25f
puneeth reddy gaddam <s561425@nwmissouri.edu>
2026-04-01 21:22:04 -0500

    Add random item selector with images

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var yurtCampsitesRouter = require('./routes/yurtcampsites');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/yurtcampsites', yurtCampsitesRouter);
 app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 
 module.exports = app;
